75aac58295a3a53a5d537b1f07129ce1d9c3ad65
Mengenes <mengene222s@gmail.com>
2025-10-15 13:33:05 +0300

    vercel fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base:"/React-WeatherApp"
+  base:process.env.VITE_BASE_PATH || "/react-weatherapp"
 })
